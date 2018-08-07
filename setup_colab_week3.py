34d05f9c0899888fc2c3eee21e3193e64d5c0475
akunfaf <42165482+akunfaf@users.noreply.github.com>
2018-08-07 14:38:48 +0700

    Update setup_colab_week3.py

--- setup_colab_week3.py
+++ setup_colab_week3.py
@@ -4,7 +4,7 @@ import os
 
 def download_github_code(path, targetFolder=""):
 	filename = path.rsplit("/")[-1]
-	filename = targetFolder + filename
+	#filename = targetFolder + filename
 	os.system("wget https://raw.githubusercontent.com/akunfaf/NLPcoursera/master/{} -O {}".format(path, filename))
 
 def setup_common():
@@ -15,7 +15,7 @@ def setup_common():
 	#download_github_code("download_utils.py")
  	download_github_code("download_utils.py", "common/")
 
-def setup_week3(){
+def setup_week3():
 	setup_common()
 	#import download_utils
-}
+
